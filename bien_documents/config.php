08c7eb79188464915135e1cf9fbdd58ea3ebf170
thecrackmandieng <moustaphadieng0405@gmail.com>
2026-01-14 11:12:37 +0000

    comm

--- bien_documents/config.php
+++ bien_documents/config.php
@@ -4,6 +4,8 @@ use Taf\TafAuth;
 
 // Handle preflight OPTIONS request for CORS
 if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
+    header("Access-Control-Allow-Origin: http://localhost:4200");
+
     header('Access-Control-Allow-Origin: *');
     header('Access-Control-Allow-Headers: Content-Type, Authorization');
     header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS');
